d594df86ace303b95f7135c946a47ed8b82287b1
Daniel Sousa <daniel.sousa@msn.com>
2024-02-29 21:59:20 -0300

    [feat] adicionado varias mudancas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +14,17 @@ export const routes: Routes = [
   {
     path: 'pagina-inicial',
     component: HomeComponent,
+  },
+  {
+    path: 'sobre-nos',
+    component: SobreNosComponent,
+  },
+  {
+    path: 'produtos',
+    component: ProdutosComponent,
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent,
   },
 ];
