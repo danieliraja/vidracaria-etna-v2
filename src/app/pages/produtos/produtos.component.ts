86e4711b57c22f8ff6350ef7eed37fa0f1b4b35e
Daniel Costa <daniel.sousa@msn.com>
2024-03-05 13:23:06 -0300

    feat: diversas correcoes e implementacoes novas para home e header

--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+interface Image {
+  src: string;
+  dir: string;
+  name: string;
+  type?: string;
+}
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './produtos.component.html',
-  styleUrl: './produtos.component.css'
+  styleUrl: './produtos.component.css',
 })
-export class ProdutosComponent {
-
+export class ProdutosComponent implements OnInit {
+  $dataList: Observable<Image[]> = this.http
+    .get('../../../assets/data/images.json')
+    .pipe((data) => data as Observable<Image[]>);
+  constructor(private readonly http: HttpClient) {}
+  ngOnInit(): void {}
 }
