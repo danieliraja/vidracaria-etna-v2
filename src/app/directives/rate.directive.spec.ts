15d5fc85d8e7fb2ef4ba1a47d23ae28b7821057b
Daniel Sousa <daniel.sousa@msn.com>
2024-03-16 11:39:09 -0300

    [feat] criado home, sobre nos

--- src/app/directives/rate.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RateDirective } from './rate.directive';
-
-describe('RateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { RateDirective } from '../../directives/rate.directive';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RateDirective],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -22,13 +21,31 @@ export class HomeComponent {
         'Ótimo atendimento. Todas as vezes que precisei fui muito bem atendida com rapidez e eficiência.',
     },
     {
-      id: 1,
+      id: 2,
       name: 'Marcus Mengão',
       rate: 5,
       image:
         'https://lh3.googleusercontent.com/a-/ALV-UjUD25LZS4Fk0CJCuGgQ8umvLLuZ5VOJulChYD3QQ7QkNjM=w60-h60-p-rp-mo-br100',
       comment:
         'Atendimento impecável do Bruno, os técnicos profissionais ao extremo! Me assessoraram corretamente. Forma de pagamento excelente. Preço justo. Farei novos serviços com a empresa.',
+    },
+    {
+      id: 3,
+      name: 'Taynah Dias',
+      rate: 5,
+      image:
+        'https://lh3.googleusercontent.com/a-/ALV-UjWb2O-eYD_3bjStgdnXcckqYgGt06BVmF0f6L8XEdGcgX0=w36-h36-p-rp-mo-br100',
+      comment:
+        'Empresa MUITO BOAAA! chegou no horário, trabalho ficou perfeito e do jeito que eu queria! Já vou fazer no outro banheiro com eles tbm!!',
+    },
+    {
+      id: 4,
+      name: 'Roberto Rocha',
+      rate: 5,
+      image:
+        'https://lh3.googleusercontent.com/a/ACg8ocJ1MAEnV03o7m8RbRUdPURKOCw_nrF9IgcQb77yLERy=w36-h36-p-rp-mo-br100',
+      comment:
+        'Atendimento impecável! Prazo atendido com precisão, preços competitivos. Certeza de indicação',
     },
   ];
   constructor(private meta: Meta, private title: Title) {
